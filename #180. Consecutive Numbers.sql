bfa0bee4b7de2a08afdf2dfa1957dea516f13de0
aijiawu <92449881+aijiawu@users.noreply.github.com>
2021-10-14 20:11:38 +0200

    Create #180. Consecutive Numbers.sql

---
+++ #180. Consecutive Numbers.sql
@@ -0,0 +1,40 @@
+#180. Consecutive Numbers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+id is the primary key for this table.
+
+Write an SQL query to find all numbers that appear at least three times consecutively.
+Return the result table in any order.
+找出連續出現三次以上的數字
+
+------------------------------------------------------------------------------
+
+
+
+------------------------------------------------------------------------------
+
+Input: 
+Logs table:
++----+-----+
+| Id | Num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
