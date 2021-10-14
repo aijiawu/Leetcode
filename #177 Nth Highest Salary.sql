ca886e39b91d137b33dcde46db3fa2b0ec901c01
aijiawu <92449881+aijiawu@users.noreply.github.com>
2021-10-14 16:09:40 +0200

    Update #177 Nth Highest Salary.sql

--- #177 Nth Highest Salary.sql
+++ #177 Nth Highest Salary.sql
@@ -1,1 +1,52 @@
+#177. Nth Highest Salary
 
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| Id          | int  |
+| Salary      | int  |
++-------------+------+
+Id is the primary key column for this table.
+Each row of this table contains information about the salary of an employee.
+
+Write an SQL query to report the nth highest salary from the Employee table. 
+If there is no nth highest salary, the query should report null.
+找出第N高的薪資
+
+-------------------------------------------------------------------------
+
+
+
+Exmple1:
+Input: 
+Employee table:
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+
+Example2:
+Input: 
+Employee table:
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| Null                   |
++------------------------+
