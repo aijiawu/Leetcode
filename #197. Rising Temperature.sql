238f6763e0610e1662e4d431d0729240554cc9b7
aijiawu <92449881+aijiawu@users.noreply.github.com>
2021-11-23 10:24:21 +0100

    Create #197. Rising Temperature.sql

---
+++ #197. Rising Temperature.sql
@@ -0,0 +1,45 @@
+#197. Rising Temperature
+
+Table: Weather
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature on a certain day.
+
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+Return the result table in any order.
+
+-------------------------------------------------------------------------------------
+
+--self-join, w1 as date from yesterday, w2 as date from today
+Select w2.id
+From Weather w1 INNER JOIN Weather w2 ON (Datediff(day,w1.recordDate,w2.recordDate)=1)
+Where w2.temperature > w1.temperature
+
+-------------------------------------------------------------------------------------
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
