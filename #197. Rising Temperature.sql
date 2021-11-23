5260fa3ee278282824d6f49a882a7b942f1e1216
aijiawu <92449881+aijiawu@users.noreply.github.com>
2021-11-23 10:27:58 +0100

    Update #197. Rising Temperature.sql

--- #197. Rising Temperature.sql
+++ #197. Rising Temperature.sql
@@ -12,11 +12,12 @@ id is the primary key for this table.
 This table contains information about the temperature on a certain day.
 
 Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
+Write an SQL query to find all dates' Id with higher temperatures compared to its previous dates (yesterday).
 Return the result table in any order.
 
 -------------------------------------------------------------------------------------
 
--- self-join, w1 as date from yesterday, w2 as date from today
+--self-join, w1 as date from yesterday, w2 as date from today
 Select w2.id
 From Weather w1 INNER JOIN Weather w2 ON (Datediff(day,w1.recordDate,w2.recordDate)=1)
 Where w2.temperature > w1.temperature
